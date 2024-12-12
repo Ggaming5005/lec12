a35b13827983d4b0258fb31cf1b96c6af5e14b2f
Ggaming5005 <ggaming5005@gmail.com>
2024-12-12 10:35:22 -0800

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.css";
+import { Link, useLocation } from "react-router-dom";
+
+const Header = () => {
+  const location = useLocation();
+  const isSlideShowPage = location.pathname.startsWith("/slideshow");
+
+  return (
+    <header>
+      <nav>
+        {/* Use absolute path for logo from public folder */}
+        <Link to="/">
+          <img className="logo" src="/images/shared/logo.svg" alt="Logo" />
+        </Link>
+        <Link className="slideLink" to={isSlideShowPage ? "/" : "/slideshow/1"}>
+          <h1>{isSlideShowPage ? "Return to gallery" : "Start slideshow"}</h1>
+        </Link>
+      </nav>
+      <div className="line"></div>
+    </header>
+  );
+};
+
+export default Header;
